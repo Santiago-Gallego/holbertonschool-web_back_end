a87af78a1105d66f2acd63a1224ef75ba185a65d
Santiago-Gallego <santiagoxd68@hotmail.com>
2021-12-07 19:07:14 -0500

    []update

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
-""" Module that holds log stats
-    Script that provides some stats about
-    Nginx logs stored in MongoDB
 """
-
+a Python script that provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
-mngclient = MongoClient()
-logs = mngclient.logs.nginx
-
-
-totallogs = logs.count_documents({})
-get = logs.count_documents({"method": "GET"})
-post = logs.count_documents({"method": "POST"})
-put = logs.count_documents({"method": "PUT"})
-patch = logs.count_documents({"method": "PATCH"})
-delete = logs.count_documents({"method": "DELETE"})
-status = logs.count_documents({"method": "GET", "path": "/status"})
 
 if __name__ == "__main__":
-    print("{} logs".format(totallogs))
-    print("Methods:")
-    print("\tmethod GET: {}".format(get))
-    print("\tmethod POST: {}".format(post))
-    print("\tmethod PUT: {}".format(put))
-    print("\tmethod PATCH: {}".format(patch))
-    print("\tmethod DELETE: {}".format(delete))
-    print("{} status check".format(status))
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    x = len(list(nginx_collection.find()))
+    print(x, "logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print(
+            "\tmethod {}: {}".format(
+                m, len(list(nginx_collection.find({"method": m})))
+                )
+            )
+    print(
+        "{} status check".format(
+            len(list(
+                nginx_collection.find({"method": "GET", "path": "/status"})
+                ))
+            )
+        )
